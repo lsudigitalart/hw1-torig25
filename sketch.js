f6ad4893fc81c7fe97eca05256c167b7829c0128
Tori Gray <tgray24@lsu.edum>
2025-09-03 05:09:15 -0500

    Finished!

---
+++ sketch.js
@@ -0,0 +1,61 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+    background(0);
+    noStroke();
+
+    fill(0);
+    ellipse(200, 200, 300, 380);
+    fill(2500);
+    ellipse(200, 200, 380, 300); 
+
+    fill(250);
+    ellipse(200, 200, 260, 340);
+    fill(0);
+    ellipse(200, 200, 340, 260); 
+
+
+    fill(250);
+    ellipse(200, 200, 300, 220);
+    fill(0);
+    ellipse(200, 200, 220, 300);
+
+    fill(0);
+    ellipse(200, 200, 260, 180);
+    fill(250);
+    ellipse(200, 200, 180, 260);
+
+    fill(250);
+    ellipse(200, 200, 220, 140);
+    fill(0);
+    ellipse(200, 200, 140, 220);
+
+
+    fill(0);
+    ellipse(200, 200, 100, 180);
+    fill(250);
+    ellipse(200, 200, 180, 100);
+
+    fill(250);
+    ellipse(200, 200, 60, 140);
+    fill(0);
+    ellipse(200, 200, 140, 60);
+
+    fill(0);
+    ellipse(200, 200, 20, 100);
+    fill(250);
+    ellipse(200, 200, 100, 20);
+
+
+    fill(0);
+    ellipse(110, 200, 30, 30);
+    fill(0);
+    ellipse(290, 200, 30, 30);
+
+    fill(250);
+    ellipse(50, 50, 30, 30);
+    fill(250);
+    ellipse(350, 350, 30, 30);
+}
